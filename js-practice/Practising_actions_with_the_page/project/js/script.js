8c251f17eaa6ada3c9e082f9fa33b37bb9f3b9d4
Alexey <alexeychamploo@gmail.com>
2024-05-18 19:54:05 +0500

    task done

--- js-practice/Practising_actions_with_the_page/project/js/script.js
+++ js-practice/Practising_actions_with_the_page/project/js/script.js
@@ -24,3 +24,34 @@ const movieDB = {
     ]
 };
 
+
+const adv = document.querySelectorAll('.promo__adv img '),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+/* adv.forEach(function (item){
+    item.remove();
+}); */
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML  += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+// a = a + 1; одно и то же
+// a += 1;
