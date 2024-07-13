12ed4a8d22e89eaf7496bbba7af9c395f52643b5
Alexey <alexeychamploo@gmail.com>
2024-07-13 13:45:31 +0500

    Acceptance module and how to use it

---
+++ lectures and base/Прием модуль и как его использовать/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const app = '123';
+
+const number = 1;
+
+// (function(){
+// Этот прием называется анонимная самовызывающаяся функция
+// }());
+
+(function(){
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
+
+console.log(number);
+
+const user = (function(){
+    const privat = function() {
+        console.log('I am privat!');
+    };
+
+    return {
+        sayHello: privat
+    };
+}());
+
+user.sayHello();
