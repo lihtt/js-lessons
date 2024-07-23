8e995756980106088c455cc1603571c77390a31b
Alexey <alexeychamploo@gmail.com>
2024-07-24 02:20:16 +0500

    Creating your own error

---
+++ lectures and base/Создание своих ошибок/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const data = [
+    {
+        id: 'box',
+        tag: 'div'
+    },
+    {
+        id: '',
+        tag: 'nav'
+    },
+    {
+        id: 'circle',
+        tag: 'span'
+    }
+]
+
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if (blockObj.id) throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    })    
+} catch(e) {
+    if (e.name === 'SyntaxError') {
+        console.log(e.message);
+    } else throw e;
+
+    // console.error(e.name);
+    // console.log(e.stack);
+}
+
+
+// const err = new Error('wsffwefw');
+// console.log(err.name, err.message, err.stack);
+
+// const err = new SyntaxError('wsffwefw');
+// console.log(err.name, err.message, err.stack);
