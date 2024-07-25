e2cf8dbd89a74755de33f1e2ec0804db419b4e7d
Alexey <alexeychamploo@gmail.com>
2024-07-25 19:23:55 +0500

    Event loop

---
+++ lectures and base/Event loop/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Ресурс, чтобы наглядно посмотреть как это все работает
+//http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
+
+
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 4000);
+
+// setTimeout(() => {
+//     console.log('timeout_4000');
+// }, 4000);
+
+// console.log(2);
+
+///////
+
+// let k = 0
+
+// function count() {
+//     for (let i = 0; i< 1e9; i++) {
+//         k++;
+//     }
+//     alert('done');
+// }
+
+// count();
+
+//////
+
+setTimeout(() => {
+    console.log(1);
+}, 0); // 0 = 4ms
+
+console.log(2);
