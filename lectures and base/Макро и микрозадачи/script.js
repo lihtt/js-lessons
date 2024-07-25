2d95a8642acaee90ca21cf5cfc828c7076d5c958
Alexey <alexeychamploo@gmail.com>
2024-07-25 19:32:15 +0500

    Macro and micro-tasks

---
+++ lectures and base/Макро и микрозадачи/script.js
@@ -0,0 +1,19 @@
+setTimeout(() => console.log('timeout'));
+
+Promise.resolve()
+    .then(() => console.log('promise'));
+
+queueMicrotask(() => console.log('wow'));
+
+Promise.resolve()
+    .then(() => console.log('promise_2'));
+
+console.log('code');
+
+// () => {}
+// microtasks: then/catch/finally/await
+// render
+// () => {}
+// microtasks: then/catch/finally/await
+// render
+// () => {}
